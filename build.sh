bfd9507400c7cfc065fa20f99c730e72248d578a
Effibot <andrea.efficace1@gmail.com>
2023-07-05 08:26:09 +0000

    reverted build.sh changes

--- build.sh
+++ build.sh
@@ -3,12 +3,12 @@ set -e
 
 # build ROS1 workspace
 echo "Building Noetic Workspace"
-env -i SHELL=/bin/zsh zsh -c "source /opt/ros/noetic/setup.zsh; if [ -e \"~/catkin_ws/devel\" ]; then source ~/catkin_ws/devel/setup.zsh; fi \
+env -i SHELL=/bin/zsh zsh -c "source /opt/ros/noetic/setup.zsh; then source ~/catkin_ws/devel/setup.zsh \
 && cd ${CATKIN_WS} && catkin build"
 
 # build ROS2 workspace
 echo "Building Foxy Workspace"
-env -i SHELL=/bin/zsh zsh -c "source /opt/ros/foxy/setup.zsh; if [ -e \"~/colcon_ws/install\" ]; then source ~/colcon_ws/install/setup.zsh; fi;  \
+env -i SHELL=/bin/zsh zsh -c "source /opt/ros/foxy/setup.zsh; source ~/colcon_ws/install/setup.zsh \
 && colcon build --merge-install --symlink-install --event-handlers console_direct+ \
 --cmake-args '-DCMAKE_BUILD_TYPE=RelWithDebInfo' '-DCMAKE_EXPORT_COMPILE_COMMANDS=On' \
 -Wall -Wextra -Wpedantic"
