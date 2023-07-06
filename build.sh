f12f541741159058ad792884b9d0e9c534814754
Effibot <andrea.efficace1@gmail.com>
2023-07-06 12:51:59 +0000

    test

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ set -e
 
 # build ROS1 workspace
 echo "Building Noetic Workspace"
-env -i SHELL=/bin/zsh zsh -c "source /opt/ros/noetic/setup.zsh; then source ~/catkin_ws/devel/setup.zsh \
+env -i SHELL=/bin/zsh zsh -c "source /opt/ros/noetic/setup.zsh; source ~/catkin_ws/devel/setup.zsh \
 && cd ${CATKIN_WS} && catkin build"
 
 # build ROS2 workspace
