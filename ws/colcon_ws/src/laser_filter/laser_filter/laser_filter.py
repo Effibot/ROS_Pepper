90ca3359d181687f93e3f90d14e7e76ccf4a71f7
effibot <andrea.efficace1@gmail.com>
2023-08-01 09:43:08 +0000

    fix to laser filter

--- ws/colcon_ws/src/laser_filter/laser_filter/laser_filter.py
+++ ws/colcon_ws/src/laser_filter/laser_filter/laser_filter.py
@@ -6,7 +6,7 @@ from std_msgs.msg import Header as header
 
 
 class LaserFilter(Node):
-    def __init__(self, msg_window_size=1):
+    def __init__(self, msg_window_size=7):
         super().__init__("laser_filter")
         # subscribe to the laser scan topic
         self.sub = self.create_subscription(LaserScan, "/laser", self.scan_callback, 10)
@@ -35,8 +35,21 @@ class LaserFilter(Node):
         }
         self.msg_header = header()
         self.msg_header.frame_id = "base_footprint"
+        # initialize the filtered laser scan msg
+        self.filtered_msg = LaserScan()
+        self.filtered_msg.angle_min = self.msg_constants["angle_min"]
+        self.filtered_msg.angle_max = self.msg_constants["angle_max"]
+        self.filtered_msg.angle_increment = self.msg_constants["angle_increment"]
+        self.filtered_msg.time_increment = self.msg_constants["time_increment"]
+        self.filtered_msg.range_min = self.msg_constants["range_min"]
+        self.filtered_msg.range_max = self.msg_constants["range_max"]
+        # ros logger
+        self.logger = self.get_logger()
+        # set the timer to publish the filtered laser scan msg
+        self.timer = self.create_timer(1., self.timer_callback)
 
     def scan_callback(self, msg):
+        # self.logger.info("Received a laser scan msg.")
         # listen to the laser scan msgs and store them in the msg window
         if len(self.msg_matrix_queue) < self.max_window_size:
             # there is still space in the current window
@@ -44,37 +57,44 @@ class LaserFilter(Node):
         elif len(self.msg_matrix_queue) == self.max_window_size:
             # the current window is full, so we need to filter the msgs
             self.filter_msgs()
+            # self.logger.info("Published a filtered laser scan msg.")
+            # move the msgs in the window one position to the right
+            self.move_window()
+            # self.logger.info(f"Moved the msgs in the msg window one position to the right. Queue size: {len(self.msg_matrix_queue)}")
         else:
             # this should never happen
-            rclpy.logerr("Error: msg_matrix_queue has more than max_window_size elements.")
+            self.logger.error("Error: msg_matrix_queue has more than max_window_size elements.")
 
     def append_msg(self, msg):
         # append the msg to the msg window
+        # self.logger.info(
+        #    f"Appending a laser scan msg to the msg window. Queue size: {len(self.msg_matrix_queue)}"
+        # )
         self.msg_matrix_queue = np.vstack((np.array(msg.ranges), self.msg_matrix_queue))
 
     def filter_msgs(self):
         """
         Filter the msgs in the msg window using a median filter, then publish
         a new laser scan msg, which is the median of the msgs in the window.
-
-        At the same time, remove the oldest msg in the window.
         """
 
         # compute the median of each column in the msg window
         medians = np.median(self.msg_matrix_queue, axis=0)
-        # create a new laser scan msg
-        filtered_msg = LaserScan()
-        filtered_msg.ranges = medians.tolist()
-        filtered_msg.angle_min = self.msg_constants["angle_min"]
-        filtered_msg.angle_max = self.msg_constants["angle_max"]
-        filtered_msg.angle_increment = self.msg_constants["angle_increment"]
-        filtered_msg.time_increment = self.msg_constants["time_increment"]
-        filtered_msg.range_min = self.msg_constants["range_min"]
-        filtered_msg.range_max = self.msg_constants["range_max"]
+        # fill the laser scan msg with the median values
+        self.filtered_msg.ranges = medians.tolist()
+        # update the msg header with the current time
         self.msg_header.stamp = self.get_clock().now().to_msg()
-        filtered_msg.header = self.msg_header
+        self.filtered_msg.header = self.msg_header
         # publish the new laser scan msg
-        self.pub.publish(filtered_msg)
+        self.pub.publish(self.filtered_msg)
+
+    def move_window(self):
+        """
+        Move the msgs in the msg window one position to the right.
+        In other words, remove the oldest msg from the msg window.
+        In our case the oldest msg is in the last row of the msg window
+        """
+        self.msg_matrix_queue = self.msg_matrix_queue[:-1, :]
 
 
 def main():
